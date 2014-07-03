8a6b53f4bf55f93cc54493f688dd660c73ed34ff
Hack Reactor Students <forrestbthomas@gmail.com>
2014-07-02 17:08:10 -0700

    implement functionality to pass tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,15 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getLinks = function(){
+    Links.getLinksFromServer()
+      .then(function(linkData){
+        $scope.data = linkData;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  $scope.getLinks();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,16 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function() {
+    Links.addLink($scope.link)
+     .then(function() {
+       $location.path('/links');
+     })
+     .catch(function (error) {
+       console.error(error);
+     });
+
+  };
+
 });
