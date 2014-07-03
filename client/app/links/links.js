5adb92837ef91919e20369be7ccf70ca09b82d30
Hack Reactor Students <forrestbthomas@gmail.com>
2014-07-03 12:13:14 -0700

    Made our App Sexy

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,6 +2,24 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.loading = false;
+  $scope.addLink = function() {
+    $scope.loading = true;
+    console.log($scope.link);
+
+    Links.addLink($scope.link)
+      .then(function() {
+        $scope.loading = false;
+
+        $scope.getLinks();
+        $scope.link.url = '';
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
+  };
   $scope.data = {};
   $scope.getLinks = function(){
     Links.getLinksFromServer()
